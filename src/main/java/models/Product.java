5ca990ee9e92616d38f5e83abd958b1059fe8894
Liar123 <krasn20161@mail.ru>
2024-07-13 03:01:29 +0300

    add jpaRepository, change ProductService

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -1,7 +1,6 @@
 package models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +10,20 @@ import org.springframework.context.annotation.Primary;
 @AllArgsConstructor
 @Entity
 @NoArgsConstructor
+@Table(name="products")
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private long id;
+
+
     private String tittle;
     private String author;
     private String city;
     private int price;
+
+    @Column(name="description",columnDefinition = "text")//указываем тип текст тк варчар 128
     private String description;
 
 }
--- src/main/java/services/ProductService.java
+++ src/main/java/services/ProductService.java
@@ -1,17 +1,30 @@
 package services;
 
+import com.example.storeproject.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import models.Product;
 
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
+    private final ProductRepository productRepository;
 
     private long ID = 0;
+
+    public List<Product> ListProducts(String author){
+        if(author != null) productRepository.findByAuthor(author);
+
+        return productRepository.findAll();
+    }
     private List<Product> products = new ArrayList<>();
     {
         products.add(new Product(1,"Example tittle","Example authort","Examle city",5000,"Example description"));
@@ -21,11 +34,17 @@ public class ProductService {
         return products;             //simple return added products
     }
     public void saveProduct(Product product){
-        product.setId(++ID);
-        products.add(product);
+//        product.setId(++ID); этот функционал выполняет @GeneratedValue
+        log.info("Saving new {}",product);//метод toString подставит сюда строковое представление product
+        //products.add(product);не нужная реализация без Jparepository
+        productRepository.save(product);
     }
     public void deleteProduct(long id){
-        products.removeIf(product -> product.getId() == id);
+//        products.removeIf(product -> product.getId() == id);
+        productRepository.deleteById(id);
+    }
+    public Product getProductById(Long id){
+        return productRepository.findById(id).orElse(null);//пробрасываем ошибку чтобы сделать соответсвие типов
     }
 
 }
