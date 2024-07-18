f466452b410b4820e862c68b1483d3a60394f3f1
Liar123 <krasn20161@mail.ru>
2024-07-18 09:37:22 +0300

    метод принимающий файл и связывающий нашу сущность image с полученным файлом,метод сохранения перерабатываю

--- src/main/java/com/example/storeproject/services/ProductService.java
+++ src/main/java/com/example/storeproject/services/ProductService.java
@@ -1,13 +1,19 @@
 package com.example.storeproject.services;
 
+import com.example.storeproject.models.Image;
 import com.example.storeproject.repositories.ProductRepository;
+import jakarta.mail.Multipart;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.example.storeproject.models.Product;
 
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,21 +35,31 @@ public class ProductService {
         if(title!=null) return productRepository.findByTitle(title);
         return productRepository.findAll();//обязательно данный метод должен быть реализован в репе
     }
-    private List<Product> products = new ArrayList<>();
-    {
-        products.add(new Product(1,"Example tittle","Example authort","Examle city",5000,"Example description"));
-        products.add(new Product(2,"Gaming platform","Sony","New York",52000,"Simple description"));
-    }
-    public List<Product> listOfAll(String title){
-        return products;             //simple return added products
-    }
+
     @Transactional
-    public void saveProduct(Product product){
-//        product.setId(++ID); этот функционал выполняет @GeneratedValue
+    public void saveProduct(Product product, MultipartFile file1,MultipartFile file2,MultipartFile file3 ) throws IOException{   //принимаем на загрузку 3 фото(ниже пример с массивом)
+        Image image1;
+        Image image2;
+        Image image3;
+        if(file1.getSize()!= 0){
+            image1 = toImageEntity(file1);
+            image1.setPreviewImage(true);
+        }
+
         log.info("Saving new {}",product);//метод toString подставит сюда строковое представление product
-        //products.add(product);не нужная реализация без Jparepository
         productRepository.save(product);
     }
+
+    private Image toImageEntity(MultipartFile file) throws IOException {
+        Image image = new Image();
+        image.setOriginalName(file.getOriginalFilename());//здесь методы тянутся от класса MultiPartFile а то я запутался :)
+        image.setSize(file.getSize());
+        image.setContentType(file.getContentType());
+        image.setBytes(file.getBytes());
+        image.setName(file.getName());
+        return image;
+    }
+
     public void deleteProduct(long id){
 //        products.removeIf(product -> product.getId() == id);
         productRepository.deleteById(id);
@@ -52,4 +68,27 @@ public class ProductService {
         return productRepository.findById(id).orElse(null);//пробрасываем ошибку чтобы сделать соответсвие типов
     }
 
+
 }
+
+
+
+
+
+
+//    private List<Product> products = new ArrayList<>();
+//    {
+//        products.add(new Product(1,"Example tittle","Example authort","Examle city",5000,"Example description"));
+//        products.add(new Product(2,"Gaming platform","Sony","New York",52000,"Simple description"));
+//    }
+//    public List<Product> listOfAll(String title){
+//        return products;             //simple return added products
+//    }
+
+
+
+//    public void saveProduct(Product product, MultipartFile[] images) {
+//        for (MultipartFile image : images) {
+//            // Обработка каждого изображения
+//        }
+//    } пример приема изображений не по одному, а массивом
